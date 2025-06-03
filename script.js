9745f5c5e8da44d351a0ef2145bc9b840d15466e
techitid <techitid0001@gmail.com>
2025-06-04 00:42:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,6 @@
     activatePlan(savedPlan);
   }
 
-
 //Responsive Navigation Toggle with Dropdown Support
   const hamburger = document.getElementById('hamburger');
   const menu = document.getElementById('menu');
